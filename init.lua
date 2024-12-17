fe1903092e8c24b311f1081f28c601157f7eb9e6
Besche, Tim <tbesche@slashwhy.de>
2024-12-17 14:57:55 +0100

    Add Telescope file browser extension

--- init.lua
+++ init.lua
@@ -433,6 +433,7 @@ require('lazy').setup({
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'file_browser')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
@@ -469,7 +470,15 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
       end, { desc = '[S]earch [N]eovim files' })
+
+      -- Add keymaps to open extensions
+      local extensions = require('telescope').extensions
+      vim.keymap.set('n', '<leader>b', extensions.file_browser.file_browser, { desc = 'Open File [B]rowser' })
     end,
+  },
+  {
+    'nvim-telescope/telescope-file-browser.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
   },
 
   -- LSP Plugins
