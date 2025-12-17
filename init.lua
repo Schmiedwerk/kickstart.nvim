d82155b6474ee6a16d86d35060e370175b1893a4
Besche, Tim <tbesche@slashwhy.de>
2025-12-17 14:09:22 +0100

    Remap Ctrl+d and Ctrl+u for granular skimming

--- init.lua
+++ init.lua
@@ -205,6 +205,10 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- Remap Ctrl+d and Ctrl+u for more granular skimming
+vim.keymap.set('n', '<C-u>', '10kzz', { noremap = true })
+vim.keymap.set('n', '<C-d>', '10jzz', { noremap = true })
+
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
